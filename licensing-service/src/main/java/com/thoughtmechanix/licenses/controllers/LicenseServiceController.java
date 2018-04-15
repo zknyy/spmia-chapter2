e757dca966a4be4da634eb22b1568afe7f700d83
demo <demo@192.168.8.138>
2018-04-15 09:57:55 +0800

    增加 EXPOSE 8080 CMD ./run.sh 和log

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -1,14 +1,18 @@
 package com.thoughtmechanix.licenses.controllers;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.thoughtmechanix.licenses.model.License;
 import com.thoughtmechanix.licenses.services.LicenseService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping(value="v1/organizations/{organizationId}/licenses")
@@ -21,11 +25,17 @@ public class LicenseServiceController {
                                 @PathVariable("licenseId") String licenseId) {
 
         //return licenseService.getLicense(licenseId);
-        return new License()
-            .withId(licenseId)
-            .withOrganizationId(organizationId)
-            .withProductName("Teleco")
-            .withLicenseType("Seat");
+
+    	Logger.getGlobal().log(Level.INFO, "organizationId:"+organizationId+"   licenseId:"+licenseId);
+        //return licenseService.getLicense(licenseId);
+    	License l = new License()
+        .withId(licenseId)
+        .withOrganizationId(organizationId)
+        .withProductName("Teleco")
+        .withLicenseType("Seat");   
+    	
+    	Logger.getGlobal().log(Level.INFO, "organizationId:"+l);
+        return  l;
     }
 
     @RequestMapping(value="{licenseId}",method = RequestMethod.PUT)
